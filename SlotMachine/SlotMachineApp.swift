e83ca85e602e671b8033fc2bd18da4dcaaf68c50
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2022-12-08 11:46:59 +0700

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Putut Yusri Bahtiar on 08/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
